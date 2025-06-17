1389ec45b434f1f40dc852c441228d509d66a919
Jeremiah <40682211+fiveminus1@users.noreply.github.com>
2025-06-16 19:50:13 -0700

    Initial Commit

---
+++ Tetsu/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Tetsu
+//
+//  Created by Jeremiah Soe on 6/16/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Tetsu/TetsuApp.swift
@@ -0,0 +1,17 @@
+//
+//  TetsuApp.swift
+//  Tetsu
+//
+//  Created by Jeremiah Soe on 6/16/25.
+//
+
+import SwiftUI
+
+@main
+struct TetsuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
