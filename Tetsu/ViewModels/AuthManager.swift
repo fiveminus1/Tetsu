2d264ff8d4b36427fd748cb2541a46999681eb56
Jeremiah <40682211+fiveminus1@users.noreply.github.com>
2025-06-17 21:55:43 -0700

    feat: firebase and authmanager

---
+++ Tetsu/ViewModels/AuthManager.swift
@@ -0,0 +1,46 @@
+//
+//  AuthManager.swift
+//  Tetsu
+//
+//  Created by Jeremiah Soe on 6/17/25.
+//
+
+import Foundation
+import FirebaseAuth
+
+class AuthManager: ObservableObject{
+    @Published var user: User? = Auth.auth().currentUser
+    private var handle: AuthStateDidChangeListenerHandle?
+    
+    init(){
+        handle = Auth.auth().addStateDidChangeListener{ _, user in
+            self.user = user
+        }
+    }
+    
+    deinit {
+        if let handle = handle {
+            Auth.auth().removeStateDidChangeListener(handle)
+        }
+    }
+    
+    func signOut(){
+        try? Auth.auth().signOut()
+    }
+    
+    func signInWithEmail(email: String, password: String){
+        Auth.auth().signIn(withEmail: email, password: password) { _, error in
+            if let error = error {
+                print("Login error: \(error.localizedDescription)")
+            }
+        }
+    }
+    
+    func signUp(email: String, password: String){
+        Auth.auth().createUser(withEmail: email, password: password){ _, error in
+            if let error = error {
+                print("Sign up error: \(error.localizedDescription)")
+            }
+        }
+    }
+}
